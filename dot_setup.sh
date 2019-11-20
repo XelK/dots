70adad658b40008e5e6eb6aa0143bdc8092ed634
XelK <haveiteasy@gmail.com>
2019-11-20 17:51:11 +0100

    updates

--- dot_setup.sh
+++ dot_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf $HOME/.dots
 git clone --separate-git-dir=$HOME/.dots https://github.com/XelK/dots.git /tmp/dots
-rsync --recursive --verbose --exclude '.git' /tmp/dots $HOME
+rsync --recursive --verbose --exclude '.git' /tmp/dots/ $HOME/
 rm -rf /tmp/dots
 /usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME config --local status.showUntrackedFiles no
 /usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME remote set-url origin git@github.com:XelK/dots.git
